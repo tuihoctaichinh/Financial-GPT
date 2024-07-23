0995550e211817c777cdbb4ca636bf5aa01bdc42
binbunbo <nhathoang.nguyen.20@gmail.com>
2024-07-23 22:49:19 +0700

    update

--- get_VCI/get_VCI_report.py
+++ get_VCI/get_VCI_report.py
@@ -13,27 +13,35 @@ if not os.path.exists(path):
     os.makedirs(desktop+'/Report/VCSC/')
     os.chdir(desktop+'/Report/VCSC/')
 
-url = 'https://www.vietcap.com.vn/api/cms-service/v1/page/analysis?is-all=true&page=0&size=200&direction=DESC&sortBy=date&language=2'
-r = requests.get(url)
-data = r.json()['data']['pagingGeneralResponses']['content']
-df = pd.DataFrame(data)
-filter_link = (~pd.isnull(df['file'])) & (df['pageLink'] != 'market-commentary')
-link_pdf = df[filter_link]
-link_pdf['fname'] = link_pdf['file'].apply(lambda x: x.split('/')[-1] if x else None)
-filter_link = (~link_pdf['fname'].str.contains('Daily')) & (~link_pdf['fname'].str.contains('monthly'))
-link_pdf = link_pdf[filter_link]
+def get_VCI_report(x):
+    url = f'https://www.vietcap.com.vn/api/cms-service/v1/page/analysis?is-all=true&page=0&size={x}&direction=DESC&sortBy=date&language=2'
+    r = requests.get(url)
+    data = r.json()['data']['pagingGeneralResponses']['content']
+    df = pd.DataFrame(data)
+    filter_link = (~pd.isnull(df['file'])) & (df['pageLink'] != 'market-commentary')
+    link_pdf = df[filter_link]
+    link_pdf['fname'] = link_pdf['file'].apply(lambda x: x.split('/')[-1] if x else None)
+    filter_link = (~link_pdf['fname'].str.contains('Daily')) & (~link_pdf['fname'].str.contains('monthly'))
+    link_pdf = link_pdf[filter_link]
+    for i in range(len(link_pdf)):
+        if link_pdf['fname'].iloc[i] not in files:
+            try:
+                url = link_pdf['file'].iloc[i]
+                r = requests.get(url)
+                with open(link_pdf['fname'].iloc[i], 'wb') as f:
+                    f.write(r.content)
+                print(f'{link_pdf["fname"].iloc[i]} is downloaded')
+            except:
+                print(f'{link_pdf["fname"].iloc[i]} error')
+        else:
+            print(f'{link_pdf["fname"].iloc[i]} exist')
+while True:
+    try:
+        num = int(input('Enter the number of report you wanna crawl: '))
+        get_VCI_report(num)
+        break
+    except:
+        print('Please enter a number')
 
-for i in range(len(link_pdf)):
-    # check if the file is already downloaded or not
-    if link_pdf['fname'].iloc[i] not in files:
-        try:
-            url = link_pdf['file'].iloc[i]
-            r = requests.get(url)
-            with open(link_pdf['fname'].iloc[i], 'wb') as f:
-                f.write(r.content)
-            print(f'{link_pdf["fname"].iloc[i]} is downloaded')
-        except:
-            print(f'{link_pdf["fname"].iloc[i]} error')
-    else:
-        print(f'{link_pdf["fname"].iloc[i]} exist')
+
 
